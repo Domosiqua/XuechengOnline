ed6515432f0bf36921bcc0922d0715f083b45f22
Domosiqua <2961777212@qq.com>
2023-08-13 02:43:44 +0800

    解决跨域，完善前后端联调

---
+++ xuecheng-plus-system/xuecheng-plus-system-api/src/main/java/com/xuecheng/system/config/GlovalCorsConfig.java
@@ -0,0 +1,30 @@
+package com.xuecheng.system.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * 跨域过滤器
+ * @author CWB
+ * @version 1.0
+ */
+@SuppressWarnings({"all"})
+@Configuration
+public class GlovalCorsConfig {
+    @Bean
+    public CorsFilter corsFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration config=new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.setAllowCredentials(true);
+        config.addAllowedHeader("*");
+        //允许所有请求方法跨域调用
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**",config);
+        return new CorsFilter(source);
+    }
+}
