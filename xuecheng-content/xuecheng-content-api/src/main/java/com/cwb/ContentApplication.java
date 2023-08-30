626b89169f00641494d9e3b031e80b6414266797
Domosiqua <2961777212@qq.com>
2023-08-30 23:44:28 +0800

    nacos配置content-api模块

--- xuecheng-content/xuecheng-content-api/src/main/java/com/cwb/ContentApplication.java
+++ xuecheng-content/xuecheng-content-api/src/main/java/com/cwb/ContentApplication.java
@@ -3,6 +3,8 @@ package com.cwb;
 import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 /**
  * @author CWB
