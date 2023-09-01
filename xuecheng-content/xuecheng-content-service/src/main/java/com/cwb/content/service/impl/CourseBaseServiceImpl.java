c9144c24c2b445340ae305842161fc69a42cfdd6
Domosiqua <2961777212@qq.com>
2023-09-01 21:47:52 +0800

    添加网关

--- xuecheng-content/xuecheng-content-service/src/main/java/com/cwb/content/service/impl/CourseBaseServiceImpl.java
+++ xuecheng-content/xuecheng-content-service/src/main/java/com/cwb/content/service/impl/CourseBaseServiceImpl.java
@@ -51,10 +51,13 @@ public class CourseBaseServiceImpl extends ServiceImpl<CourseBaseMapper, CourseB
         //测试查询接口
         LambdaQueryWrapper<CourseBase> queryWrapper = new LambdaQueryWrapper<>();
         //根据课程名称模糊查询  name like '%名称%'
-        queryWrapper.like(StringUtils.isNotEmpty(queryCourseParams.getCourseName()),CourseBase::getName,queryCourseParams.getCourseName());
-        //根据课程审核状态
-        queryWrapper.eq(StringUtils.isNotEmpty(queryCourseParams.getAuditStatus()),CourseBase::getAuditStatus,queryCourseParams.getAuditStatus());
-        queryWrapper.eq(StringUtils.isNotEmpty(queryCourseParams.getStatus()),CourseBase::getStatus,queryCourseParams.getStatus());
+        if(queryCourseParams!=null){
+            queryWrapper.like(StringUtils.isNotEmpty(queryCourseParams.getCourseName()),CourseBase::getName,queryCourseParams.getCourseName());
+            //根据课程审核状态
+            queryWrapper.eq(StringUtils.isNotEmpty(queryCourseParams.getAuditStatus()),CourseBase::getAuditStatus,queryCourseParams.getAuditStatus());
+            queryWrapper.eq(StringUtils.isNotEmpty(queryCourseParams.getStatus()),CourseBase::getStatus,queryCourseParams.getStatus());
+
+        }
 
         Page<CourseBase> page = new Page<>(pageParams.getPageNo(), pageParams.getPageSize());
 
