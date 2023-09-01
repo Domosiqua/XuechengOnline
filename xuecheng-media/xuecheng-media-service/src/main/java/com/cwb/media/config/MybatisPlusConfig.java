1223f143ab6df495686f0bc6c3f8fbebe1699728
Domosiqua <2961777212@qq.com>
2023-09-01 22:49:47 +0800

    添加媒资模块

--- xuecheng-media/xuecheng-media-service/src/main/java/com/cwb/media/config/MybatisPlusConfig.java
+++ xuecheng-media/xuecheng-media-service/src/main/java/com/cwb/media/config/MybatisPlusConfig.java
@@ -1,4 +1,4 @@
-package com.xuecheng.system.config;
+package com.cwb.media.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.Configuration;
  * </p>
  */
 @Configuration
-@MapperScan("com.xuecheng.system.mapper")
+@MapperScan("com.cwb.media.mapper")
 public class MybatisPlusConfig {
 	/**
 	 * 新的分页插件
