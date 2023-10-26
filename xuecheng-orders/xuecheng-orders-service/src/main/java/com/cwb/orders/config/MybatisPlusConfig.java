5765cadac9a9839f93a90e44aababa3711de9af2
Domosiqua <2961777212@qq.com>
2023-10-26 22:22:38 +0800

    fix:选课bug 添加支付宝支付模块

--- xuecheng-orders/xuecheng-orders-service/src/main/java/com/cwb/orders/config/MybatisPlusConfig.java
+++ xuecheng-orders/xuecheng-orders-service/src/main/java/com/cwb/orders/config/MybatisPlusConfig.java
@@ -1,11 +1,15 @@
 package com.cwb.orders.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.apache.ibatis.reflection.MetaObject;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
 
 /**
  * <P>
@@ -13,7 +17,7 @@ import org.springframework.context.annotation.Configuration;
  * </p>
  */
 @Configuration
-@MapperScan("com.xuecheng.orders.mapper")
+@MapperScan("com.cwb.orders.mapper")
 public class MybatisPlusConfig {
 	/**
 	 * 新的分页插件
@@ -26,6 +30,22 @@ public class MybatisPlusConfig {
 		interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
 		return interceptor;
 	}
+	@Bean
+	public MetaObjectHandler metaObjectHandler() {
+		return new MyMetaObjectHandler();
+	}
+	public static class MyMetaObjectHandler implements MetaObjectHandler {
 
+		@Override
+		public void insertFill(MetaObject metaObject) {
+			this.setFieldValByName("createDate", LocalDateTime.now(), metaObject);
+			this.setFieldValByName("changeDate", LocalDateTime.now(), metaObject);
+		}
 
+		@Override
+		public void updateFill(MetaObject metaObject) {
+			this.setFieldValByName("changeDate", LocalDateTime.now(), metaObject);
+		}
+
+	}
 }
